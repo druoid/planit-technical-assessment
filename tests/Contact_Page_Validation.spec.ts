605326d2bf9a35e7fead65920d6d41fc7e2b129e
Andrew <pacificurb@gmail.com>
2025-07-17 08:59:01 +1200

    Add describe to contact tests

--- tests/Contact_Page_Validation.spec.ts
+++ tests/Contact_Page_Validation.spec.ts
@@ -2,36 +2,38 @@ import { test } from '@playwright/test';
 import { HomePage } from '../pages/homePage';
 import { ContactPage } from '../pages/contactPage';
 
-test('Contact page - Verify error messages on incomplete form submission', async ({
-  page,
-}) => {
-  const homePage = new HomePage(page);
-  const contactPage = new ContactPage(page);
+test.describe('Test contact page', () => {
+  test('Contact page - Verify error messages on incomplete form submission', async ({
+    page,
+  }) => {
+    const homePage = new HomePage(page);
+    const contactPage = new ContactPage(page);
 
-  await homePage.navigateToHomePage();
-  await homePage.navigateToContactPage();
+    await homePage.navigateToHomePage();
+    await homePage.navigateToContactPage();
 
-  await contactPage.submitContactPageFormButton();
-  await contactPage.verifyHeaderMessageError();
-  await contactPage.verifyForenameInputMissingError();
-  await contactPage.verifyEmailInpuMissingError();
-  await contactPage.verifyMessageInputMissingError();
+    await contactPage.submitContactPageFormButton();
+    await contactPage.verifyHeaderMessageError();
+    await contactPage.verifyForenameInputMissingError();
+    await contactPage.verifyEmailInpuMissingError();
+    await contactPage.verifyMessageInputMissingError();
 
-  await contactPage.populateMandatoryFields();
-  await contactPage.submitContactPageFormButton();
-  await contactPage.waitForProgressBarOnFormSubmission();
-  await contactPage.verifyAppreciationMessage();
-});
+    await contactPage.populateMandatoryFields();
+    await contactPage.submitContactPageFormButton();
+    await contactPage.waitForProgressBarOnFormSubmission();
+    await contactPage.verifyAppreciationMessage();
+  });
 
-test('Contact page - Verify complete form submission', async ({ page }) => {
-  const homePage = new HomePage(page);
-  const contactPage = new ContactPage(page);
+  test('Contact page - Verify complete form submission', async ({ page }) => {
+    const homePage = new HomePage(page);
+    const contactPage = new ContactPage(page);
 
-  await homePage.navigateToHomePage();
-  await homePage.navigateToContactPage();
+    await homePage.navigateToHomePage();
+    await homePage.navigateToContactPage();
 
-  await contactPage.populateMandatoryFields();
-  await contactPage.submitContactPageFormButton();
-  await contactPage.waitForProgressBarOnFormSubmission();
-  await contactPage.verifyAppreciationMessage();
+    await contactPage.populateMandatoryFields();
+    await contactPage.submitContactPageFormButton();
+    await contactPage.waitForProgressBarOnFormSubmission();
+    await contactPage.verifyAppreciationMessage();
+  });
 });
